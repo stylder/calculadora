ee7c7df9521c874f63a9b232fc65741555f9d686
Santiago García Cabral <stylder@gmail.com>
2016-10-10 12:31:46 -0500

    probando estas cosas

--- calculadora/calculadora.py
+++ calculadora/calculadora.py
@@ -1,6 +1,7 @@
 class Calculadora:
+
     def suma(self, num1, num2):
         try:
             return int(num1) + int(num2)
-        except Exception  as e:
+        except Exception:
             return "Error"
--- calculadora/tests/features/steps.py
+++ calculadora/tests/features/steps.py
@@ -2,15 +2,19 @@
 from lettuce import step, world
 from calculadora import Calculadora
 
+
 @step(u'Dado que tengo el número "([^"]*)" y "([^"]*)"')
 def dado_que_tengo_el_numero_group1_y_group1(step, num1, num2):
     cal = Calculadora()
-    world.resultado = str(cal.suma((num1),(num2)))
+    world.resultado = str(cal.suma((num1), (num2)))
+
 
 @step(u'cuando realizo la suma')
 def cuando_realizo_la_suma(step):
     pass
 
+
 @step(u'entonces el resultado que obtengo es "([^"]*)"')
 def entonces_el_resultado_que_obtengo_es_group1(step, esperado):
-    assert esperado==world.resultado, 'El resultado '+esperado+' no es el esperado.'
+    assert esperado == world.resultado, 'El resultado ' + \
+        esperado + ' no es el esperado.'
